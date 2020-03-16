79d1eeff9a8bbcf08520ced112c452a78c2b5f92
KMimura <mimurakohei256@gmail.com>
2020-03-16 12:08:53 +0900

    fixed the dockerfile

--- api/api/main.go
+++ api/api/main.go
@@ -1,42 +1,47 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "log"
-    "os"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    log_config()
+	logConfig()
 
-    r := gin.Default()
-    r.GET("/accounting-api",func(c *gin.Context) {
-        c.JSON(200, gin.H{
-	     "state":"success",
+	r := gin.Default()
+	r.GET("/accounting-api", func(c *gin.Context) {
+		log.Println("GET")
+		c.JSON(200, gin.H{
+			"state": "success",
+		})
 	})
-    })
-    r.POST("/accounting-api",func(c *gin.Context) {
-        c.JSON(200, gin.H{
-             "state":"success",
-        })
-    })
-    r.PUT("/accounting-api",func(c *gin.Context) {
-        c.JSON(200, gin.H{
-             "state":"success",
-        })
-    })
-    r.DELETE("/accounting-api",func(c *gin.Context) {
-        c.JSON(200, gin.H{
-             "state":"success",
-        })
-    })
-    log.Println("Start Server")
-    r.Run()
+	r.POST("/accounting-api", func(c *gin.Context) {
+		log.Println("POST")
+		c.JSON(200, gin.H{
+			"state": "success",
+		})
+	})
+	r.PUT("/accounting-api", func(c *gin.Context) {
+		log.Println("PUT")
+		c.JSON(200, gin.H{
+			"state": "success",
+		})
+	})
+	r.DELETE("/accounting-api", func(c *gin.Context) {
+		log.Println("DELETE")
+		c.JSON(200, gin.H{
+			"state": "success",
+		})
+	})
+	log.Println("Start Server")
+	r.Run(":8080")
 }
 
-func log_config() {
-    logFile, _ := os.OpenFile("log/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-    log.SetOutput(logFile)
-    log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
-    log.SetPrefix("[LOG] ")
+func logConfig() {
+	logFile, _ := os.OpenFile("log/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+	log.SetPrefix("[LOG] ")
 }
