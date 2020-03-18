122f551f9d341d1e4fb1fd40a8ed9cf0c0059ee9
KMimura <mimurakohei256@gmail.com>
2020-03-18 09:55:13 +0900

    use environment variables in api

--- api/api/main.go
+++ api/api/main.go
@@ -7,30 +7,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type mysqlEnv struct {
+	database string
+	user     string
+	password string
+}
+
 func main() {
 	logConfig()
+	env := loadEnvVariables()
 
 	r := gin.Default()
 	r.GET("/accounting-api", func(c *gin.Context) {
-		log.Println("GET")
+		getMethod(c, env)
 		c.JSON(200, gin.H{
 			"state": "success",
 		})
 	})
 	r.POST("/accounting-api", func(c *gin.Context) {
-		log.Println("POST")
+		postMethod(c, env)
 		c.JSON(200, gin.H{
 			"state": "success",
 		})
 	})
 	r.PUT("/accounting-api", func(c *gin.Context) {
-		log.Println("PUT")
+		putMethod(c, env)
 		c.JSON(200, gin.H{
 			"state": "success",
 		})
 	})
 	r.DELETE("/accounting-api", func(c *gin.Context) {
-		log.Println("DELETE")
+		deleteMethod(c, env)
 		c.JSON(200, gin.H{
 			"state": "success",
 		})
@@ -39,9 +46,35 @@ func main() {
 	r.Run(":8080")
 }
 
+// logConfig ログの諸設定を行う
 func logConfig() {
 	logFile, _ := os.OpenFile("log/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	log.SetOutput(logFile)
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 	log.SetPrefix("[LOG] ")
+}
+
+// loadEnvVariables 環境変数を読み込む
+func loadEnvVariables() *mysqlEnv {
+	var env mysqlEnv
+	env.database = os.Getenv("MYSQL_DATABASE")
+	env.user = os.Getenv("MYSQL_USER")
+	env.password = os.Getenv("MYSQL_PASSWORD")
+	return &env
+}
+
+func getMethod(c *gin.Context, env *mysqlEnv) {
+
+}
+
+func postMethod(c *gin.Context, env *mysqlEnv) {
+
+}
+
+func putMethod(c *gin.Context, env *mysqlEnv) {
+
+}
+
+func deleteMethod(c *gin.Context, env *mysqlEnv) {
+
 }
